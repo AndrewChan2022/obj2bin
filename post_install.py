e5228c962c6475df8863522d5ffbc1d2899926ae
AndrewChan <34884018+AndrewChan2022@users.noreply.github.com>
2024-12-01 18:20:24 +0800

    readme

--- post_install.py
+++ post_install.py
@@ -3,25 +3,36 @@ import platform
 import subprocess
 
 current_path = os.getenv('TESTPATH') or ""
-
+if platform.system() == 'Windows':
+    current_path = current_path.replace("/", "\\")
 print("current_path:", current_path)
 
 new_directory = "C:/Program Files/obj2bin/bin"
+if platform.system() == 'Windows':
+    new_directory = new_directory.replace("/", "\\")
 
 separator = ';' if platform.system() == 'Windows' else ":"
-separator = "" if current_path == "" else separator
+if current_path == "":
+    separator = ""
+if current_path.endswith(";") or current_path.endswith(":"):
+    separator = ""
 
-new_path = current_path + separator + new_directory
+if new_directory not in current_path:
+    new_path = current_path + separator + new_directory
+else:
+    new_path = current_path
 
 os.environ['TESTPATH'] = new_path
 
 # os.environ['TESTPATH'] = "C:/Program Files/Notepad++"
 
-# if platform.system() == 'Windows':
-#     subprocess.run(f'setx PATH "%PATH%;{new_path}"', shell=True)
-# else:
-#     shell_config_file = os.path.expanduser("~/.bashrc")
-#     with open(shell_config_file, 'a') as file:
-#         file.write(f'\nexport PATH="$PATH:{new_path}"\n')
+if platform.system() == 'Windows':
+    # subprocess.run(f'setx TESTPATH "{new_path}"', shell=True)
+    subprocess.run(f'setx TESTPATH "{new_path}"', shell=True)
+else:
+    ...
+    # shell_config_file = os.path.expanduser("~/.bashrc")
+    # with open(shell_config_file, 'a') as file:
+    #     file.write(f'\nexport TESTPATH="$TESTPATH:{new_path}"\n')
 
-print("new_path", os.getenv('PATH'))
+print("new_path", os.getenv('TESTPATH'))
